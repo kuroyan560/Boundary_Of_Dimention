502e6303152167a42fff705332c9e68510c41a92
LC1A_07_オオイシカズノリ <k020g1151@g.neec.ac.jp>
2023-04-23 01:02:36 +0900

    ステージ番号選択時の仕様を変更

--- HANAHIME/Game/src/OutGame/PazzleStageSelect.cpp
+++ HANAHIME/Game/src/OutGame/PazzleStageSelect.cpp
@@ -5,8 +5,6 @@
 
 PazzleStageSelect::PazzleStageSelect()
 {
-	m_baseStageSelectPos = KuroEngine::WinApp::Instance()->GetExpandWinCenter() / 2.0f;
-
 	const int STAGE_MAX_NUM = StageManager::Instance()->GetAllStageNum();
 	int yNum = 0;
 	for (int y = 0; y < m_stageSelectArray.size(); ++y)
@@ -25,6 +23,13 @@ PazzleStageSelect::PazzleStageSelect()
 
 	m_numTexArray.resize(10);
 	KuroEngine::D3D12App::Instance()->GenerateTextureBuffer(m_numTexArray.data(), "resource/user/tex/Number.png", 10, { 10,1 });
+
+	m_selectTex = KuroEngine::D3D12App::Instance()->GenerateTextureBuffer(KuroEngine::Color(255, 255, 255, 255));
+
+	float bandSize = 100.0f;
+
+	m_bandArray[0] = std::make_unique<Band>(KuroEngine::Vec2<float>(0.0f, bandSize), KuroEngine::WinApp::Instance()->GetExpandWinSize() * KuroEngine::Vec2<float>(1.0f, -1.0f), KuroEngine::Vec2<float>(0.0f, -bandSize), 60.0f);
+	m_bandArray[1] = std::make_unique<Band>(KuroEngine::Vec2<float>(0.0f, KuroEngine::WinApp::Instance()->GetExpandWinSize().y - bandSize), KuroEngine::WinApp::Instance()->GetExpandWinSize(), KuroEngine::Vec2<float>(0.0f, bandSize), 60.0f);
 }
 
 void PazzleStageSelect::Init()
@@ -43,22 +48,22 @@ void PazzleStageSelect::Update()
 	KuroEngine::Vec2<float> contollerLeftStickInput = KuroEngine::UsersInput::Instance()->GetLeftStickVecFuna(0);
 
 	const float DEADLINE = 0.8f;
-	bool isInputRightController = m_prevContollerLeftStick.x < DEADLINE && DEADLINE < contollerLeftStickInput.x;
+	bool isInputRightController = m_prevContollerLeftStick.x < DEADLINE &&DEADLINE < contollerLeftStickInput.x;
 	if (KuroEngine::UsersInput::Instance()->KeyOnTrigger(DIK_RIGHT) || isInputRightController)
 	{
 		++m_nowStageNum.x;
 	}
-	bool isInputLeftController = -DEADLINE < m_prevContollerLeftStick.x && contollerLeftStickInput.x < -DEADLINE;
+	bool isInputLeftController = -DEADLINE < m_prevContollerLeftStick.x &&contollerLeftStickInput.x < -DEADLINE;
 	if (KuroEngine::UsersInput::Instance()->KeyOnTrigger(DIK_LEFT) || isInputLeftController)
 	{
 		--m_nowStageNum.x;
 	}
-	bool isInputUpController = -DEADLINE < m_prevContollerLeftStick.y && contollerLeftStickInput.y < -DEADLINE;
+	bool isInputUpController = -DEADLINE < m_prevContollerLeftStick.y &&contollerLeftStickInput.y < -DEADLINE;
 	if (KuroEngine::UsersInput::Instance()->KeyOnTrigger(DIK_UP) || isInputUpController)
 	{
 		--m_nowStageNum.y;
 	}
-	bool isInputDownController = m_prevContollerLeftStick.y < DEADLINE && DEADLINE < contollerLeftStickInput.y;
+	bool isInputDownController = m_prevContollerLeftStick.y < DEADLINE &&DEADLINE < contollerLeftStickInput.y;
 	if (KuroEngine::UsersInput::Instance()->KeyOnTrigger(DIK_DOWN) || isInputDownController)
 	{
 		++m_nowStageNum.y;
@@ -109,23 +114,41 @@ void PazzleStageSelect::Update()
 	{
 		bool debug = false;
 	}
+
+	for (auto &obj : m_bandArray)
+	{
+		if (KuroEngine::UsersInput::Instance()->KeyInput(DIK_L))
+		{
+			obj->Appear();
+		}
+		obj->Update();
+	}
 }
 
 void PazzleStageSelect::Draw()
 {
+	m_baseStageSelectPos = { 200.0f,64.0f };
+
+
+	for (auto &obj : m_bandArray)
+	{
+		obj->Draw();
+	}
+
+
+	KuroEngine::DrawFunc2D::DrawRotaGraph2D(m_baseStageSelectPos + KuroEngine::Vec2<float>(10.0f, 25.0f), KuroEngine::Vec2<float>(128.0f, 128.0f), 0.0f, m_selectTex, 1.0f);
+
 	//Y軸を考慮した数字のカウントに必要
 	int yNum = 0;
 
-	KuroEngine::Vec2<float>texSize(64.0f, 64.0f);
+	KuroEngine::Vec2<float>texSize(64.0f * 2.0f, 64.0f);
 	//数字の描写
 	for (int y = 0; y < m_stageSelectArray.size(); ++y)
 	{
 		for (int x = 0; x < m_stageSelectArray[y].size(); ++x)
 		{
 			KuroEngine::Vec2<float>pos(static_cast<float>(x), static_cast<float>(y));
-			int stageNumber = 1 + yNum + x;
-			//桁用意
-			std::vector<int>timeArray = CountNumber(stageNumber);
+			int stageNumber = yNum + x;
 
 			float numberAlpha = 1.0f;
 			if (!m_stageSelectArray[y][x].enableFlag)
@@ -133,25 +156,43 @@ void PazzleStageSelect::Draw()
 				numberAlpha = 0.5f;
 			}
 
-			//一桁
-			if (stageNumber < 10)
+
+			//選択中の番号からみて遠ければ遠いほど透明にしていく
+			if (GetNumber() <= stageNumber|| stageNumber < GetNumber() + 10)
 			{
-				KuroEngine::DrawFunc2D::DrawGraph(m_baseStageSelectPos + pos * texSize, m_numTexArray[timeArray[1]], numberAlpha);
+				numberAlpha = 1.0f - abs((stageNumber - GetNumber()) / 10.0f);
 			}
-			//二桁
-			else
+
+
+			KuroEngine::Vec2<float>basePos(pos * texSize + m_baseStageSelectPos);
+			KuroEngine::Vec2<float>size(1.0f, 1.0f);
+			//桁の間のスペース
+			KuroEngine::Vec2<float>digitsBetween(30.0f, 0.0f);
+			//選択中の数字は強調させる
+			bool isSelectingFlag = GetNumber() == stageNumber;
+			if (isSelectingFlag)
 			{
-				KuroEngine::Vec2<float>basePos(pos * texSize + m_baseStageSelectPos);
-				//桁の間を真ん中に持っていく処理
-				basePos -= KuroEngine::Vec2<float>(15.0f, 0.0f);
-				KuroEngine::DrawFunc2D::DrawGraph(basePos, m_numTexArray[timeArray[0]], numberAlpha);
-				KuroEngine::DrawFunc2D::DrawGraph(basePos + KuroEngine::Vec2<float>(30.0f, 0.0f), m_numTexArray[timeArray[1]], numberAlpha);
+				size = { 2.0f,2.0f };
+				digitsBetween = { 50.0f,0.0f };
+				basePos.y += 30.0f;
 			}
+			//桁の間を真ん中に持っていく処理
+			basePos -= { 15.0f, 0.0f };
+			//選択中の数字の裏に描画する為の座標を保存する
+			KuroEngine::Vec2<float>selectingTexPos = basePos;
+
+			//選択中の数字を基準に全ての数字をずらす。
+			basePos.x -= GetNumber() * 128.0f;
+
+			//桁用意
+			std::vector<int>timeArray = CountNumber(stageNumber + 1);
+			KuroEngine::DrawFunc2D::DrawRotaGraph2D(basePos, size, 0.0f, m_numTexArray[timeArray[0]], numberAlpha);
+			KuroEngine::DrawFunc2D::DrawRotaGraph2D(basePos + digitsBetween, size, 0.0f, m_numTexArray[timeArray[1]], numberAlpha);
 		}
 		yNum += static_cast<int>(m_stageSelectArray[y].size());
 	}
-	//現在選択中
-	KuroEngine::DrawFunc2D::DrawBox2D(m_baseStageSelectPos + m_nowStageNum.Float() * texSize, m_baseStageSelectPos + m_nowStageNum.Float() * texSize + texSize, KuroEngine::Color(255, 255, 0, 255), false);
+
+
 }
 
 int PazzleStageSelect::GetNumber()
--- HANAHIME/Game/src/OutGame/PazzleStageSelect.h
+++ HANAHIME/Game/src/OutGame/PazzleStageSelect.h
@@ -6,6 +6,7 @@
 #include"Select.h"
 #include"ForUser/DrawFunc/2D/DrawFunc2D.h"
 #include"DirectX12/D3D12App.h"
+#include"ForUser/Timer.h"
 
 /// <summary>
 /// タイトルから直接ステージを選択する
@@ -37,7 +38,7 @@ private:
 		StageData() :m_isClearFlag(false)
 		{};
 	};
-	std::array<std::array<StageData, 4>, 4>m_stageSelectArray;
+	std::array<std::array<StageData, 30>, 1>m_stageSelectArray;
 	KuroEngine::Vec2<int> m_nowStageNum;
 
 	KuroEngine::Vec2<float> m_prevContollerLeftStick;
@@ -73,4 +74,58 @@ private:
 	//ステージ選択画面を何処に描画するか(左上座標)
 	KuroEngine::Vec2<float> m_baseStageSelectPos;
 	bool m_stopFlag;
+
+	std::shared_ptr<KuroEngine::TextureBuffer>m_selectTex;
+
+
+
+
+	//上下の帯
+	class Band
+	{
+	public:
+
+		Band(const KuroEngine::Vec2<float> &pos, const KuroEngine::Vec2<float> &size, const KuroEngine::Vec2<float> &easeVel, float flame) :
+			m_pos(pos), m_size(size), m_easeVel(easeVel), m_appearTimer(flame), m_disappearTimer(flame), m_appearFlag(true)
+		{
+		};
+
+		void Update()
+		{
+			if (m_appearFlag)
+			{
+				m_disappearTimer.Reset();
+				m_hideVel = KuroEngine::Math::Ease(KuroEngine::Out, KuroEngine::Circ, m_appearTimer.GetTimeRate(), KuroEngine::Vec2<float>(0.0f, 0.0f), m_easeVel);
+				m_appearTimer.UpdateTimer();
+			}
+			else
+			{
+				m_appearTimer.Reset();
+				m_hideVel = KuroEngine::Math::Ease(KuroEngine::Out, KuroEngine::Circ, m_disappearTimer.GetTimeRate(), m_easeVel, KuroEngine::Vec2<float>(0.0f, 0.0f));
+				m_disappearTimer.UpdateTimer();
+			}
+			m_appearFlag = false;
+		};
+
+		void Draw()
+		{
+			KuroEngine::DrawFunc2D::DrawBox2D(m_pos + m_hideVel, m_pos + m_size, KuroEngine::Color(18, 43, 38, 255), true);
+		};
+
+		void Appear()
+		{
+			m_appearFlag = true;
+		};
+
+	private:
+		KuroEngine::Vec2<float> m_pos;
+		KuroEngine::Vec2<float> m_size;
+		KuroEngine::Vec2<float> m_easeVel;
+		KuroEngine::Vec2<float> m_hideVel;
+		KuroEngine::Timer m_appearTimer;
+		KuroEngine::Timer m_disappearTimer;
+
+		bool m_appearFlag;
+	};
+	std::array<std::unique_ptr<Band>, 2>m_bandArray;
 };
