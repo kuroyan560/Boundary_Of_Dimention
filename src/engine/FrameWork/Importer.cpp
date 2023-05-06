9377e01122a1d8970627b866dffc5c6989c568a5
K-rin <kingukarin@gmail.com>
2023-05-06 15:57:22 +0900

    glTFの値をそのまま使用。左右反転してしまっている

--- src/engine/FrameWork/Importer.cpp
+++ src/engine/FrameWork/Importer.cpp
@@ -68,8 +68,8 @@ void KuroEngine::Importer::LoadGLTFPrimitive(ModelMesh& ModelMesh, const Microso
 		int jid0 = 4 * vertIdx, jid1 = 4 * vertIdx + 1, jid2 = 4 * vertIdx + 2, jid3 = 4 * vertIdx + 3;
 
 		KuroEngine::ModelMesh::Vertex vertex;
-		vertex.pos = { -vertPos[vid0],vertPos[vid1],vertPos[vid2] };
-		vertex.normal = { -vertNrm[vid0],vertNrm[vid1],vertNrm[vid2] };
+		vertex.pos = { vertPos[vid0],vertPos[vid1],vertPos[vid2] };
+		vertex.normal = { vertNrm[vid0],vertNrm[vid1],vertNrm[vid2] };
 		vertex.uv = { vertUV[tid0],vertUV[tid1] };
 
 		if (!vertWeight.empty())
@@ -116,8 +116,8 @@ void KuroEngine::Importer::LoadGLTFPrimitive(ModelMesh& ModelMesh, const Microso
 	auto idxCount = accIndex.count;
 	for (int i = 0; i < idxCount; i += 3)
 	{
+		ModelMesh.mesh->indices.emplace_back(static_cast<unsigned int>(indices[i + 0]));
 		ModelMesh.mesh->indices.emplace_back(static_cast<unsigned int>(indices[i + 1]));
-		ModelMesh.mesh->indices.emplace_back(static_cast<unsigned int>(indices[i]));
 		ModelMesh.mesh->indices.emplace_back(static_cast<unsigned int>(indices[i + 2]));
 	}
 }
