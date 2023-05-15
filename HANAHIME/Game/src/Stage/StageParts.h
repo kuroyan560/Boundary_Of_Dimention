0dc2eeb2e2ce736b853658944cc85f0f81892d2a
Funakurabe <k020g1164@g.neec.ac.jp>
2023-05-16 00:56:53 +0900

    IvyBlockのサイズを調整

--- HANAHIME/Game/src/Stage/StageParts.h
+++ HANAHIME/Game/src/Stage/StageParts.h
@@ -405,7 +405,7 @@ class IvyBlock : public StageParts
 
 	const float HIT_SCALE_MIN = 7.5f * 2.0f;
 	const float HIT_SCALE_MAX = 15.5f * 2.0f;
-	const float SCALE_DEF = 7.5f;
+	const float SCALE_DEF = 15.0f;
 
 	std::weak_ptr<KuroEngine::Model>m_collisionModel;
 
