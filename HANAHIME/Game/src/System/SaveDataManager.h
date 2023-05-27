8b128f0a12da0c0976299acedb3ddb60c98c4fb6
K-rin <kingukarin@gmail.com>
2023-05-28 02:27:17 +0900

    セーブデータの構造体定義

--- HANAHIME/Game/src/System/SaveDataManager.h
+++ HANAHIME/Game/src/System/SaveDataManager.h
@@ -5,7 +5,17 @@ class SaveDataManager : public KuroEngine::DesignPattern::Singleton<SaveDataMana
 	friend class KuroEngine::DesignPattern::Singleton<SaveDataManager>;
 	SaveDataManager() {}
 
+	struct SaveData
+	{
+		//たどり着いたステージ
+		int m_reachStageNum;
+		//最後にたどり着いたチェックポイントのID（CheckPointOrder）
+		int m_reachCheckPointOrder;
+	}m_saveData;
+
 public:
+	void Save(int arg_checkPointOrder);
+
 	//セーブデータが存在するか
 	bool IsExistSaveData() { return false; }
 };
